fa27ee097712dbd90ff6ca037e765bfaa7438eaf
Ojas_singh <singhojas007@gmail.com>
2024-02-07 13:29:58 +0000

    Update GlycoShape_zip.sh

--- GlycoShape_zip.sh
+++ GlycoShape_zip.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-rm /mnt/database/DB_temp/GlycoShape.zip
+rm DB_temp/GlycoShape.zip
 
-for directory in /mnt/database/DB_temp/*/
+for directory in DB_temp/*/
 do
 
 # Making a zipped file for each glycan
@@ -15,9 +15,9 @@ zip -r $directory/$glycan.zip $glycan.json $glycan.svg PDB_format_HETATM CHARMM_
 done
 
 # Making a zipped file of each zipped file, with some file movement to allow us to delete all zipped apart from our one of interest
-cd /mnt/database/DB_temo/
-cp /mnt/database/DB_temp/*/*zip .
-zip /mnt/database/DB_temp/GlycoShape.zip *zip
+cd DB_temp/
+cp DB_temp/*/*zip .
+zip DB_temp/GlycoShape.zip *zip
 mv GlycoShape.zip ..
 rm *zip
 mv ../GlycoShape.zip .
